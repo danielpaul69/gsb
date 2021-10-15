44b94f0dfc1597c66e6965c4e3dfe06e98de990d
Daniel Paul <daniel.paul@codgency.fr>
2021-10-15 17:23:02 +0200

    Suppression

--- utilisateurs/liste.php
+++ utilisateurs/liste.php
@@ -16,7 +16,14 @@
         $requete->bindParam(":dateNaissance", $_POST['dateNaissance']);
         $requete->bindParam(":dateEmbauche", $_POST['dateEmbauche']);
         $requete->bindParam(":roleId", $_POST['role']);
-        var_dump($requete->execute());
+        $requete->execute();
+    }
+
+    if (isset($_POST['mode']) && $_POST['mode'] == 'suppression') {
+        $sql = 'DELETE FROM `utilisateur` WHERE id = :id';
+        $requete = $bdd->prepare($sql);
+        $requete->bindParam(":id", $_POST['idUtilisateur']);
+        $requete->execute();
     }
     
     $reponse = $bdd->query('
@@ -40,34 +47,34 @@
     <!-- Modal : Formulaire de création -->
     <form method="POST" action="">
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Nouvel utilisateur</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="container">
-                    <input type="text" class="form-control mb-3" name="prenom" placeholder="Prénom" required>
-                    <input type="text" class="form-control mb-3" name="nom" placeholder="Nom" required>
-                    <input type="text" class="form-control mb-3" name="email" placeholder="Email" required>
-                    <input type="text" class="form-control mb-3" name="identifiant" placeholder="Identifiant" required>
-                    <input type="password" class="form-control mb-3" name="motDePasse" placeholder="Mot de passe" required>
-                    <input type="date" class="form-control mb-3" name="dateNaissance" placeholder="Date de naissance" required>
-                    <input type="date" class="form-control mb-3" name="dateEmbauche" placeholder="Date d'embauche" required>
-                    <select class="form-select" name="role" placeholder="Role" required>
-                        <option value="1">Visiteur</option>
-                        <option value="2">Comptable</option>
-                        <option value="3">Administrateur</option>
-                    </select>
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Nouvel utilisateur</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="container">
+                        <input type="text" class="form-control mb-3" name="prenom" placeholder="Prénom" required>
+                        <input type="text" class="form-control mb-3" name="nom" placeholder="Nom" required>
+                        <input type="text" class="form-control mb-3" name="email" placeholder="Email" required>
+                        <input type="text" class="form-control mb-3" name="identifiant" placeholder="Identifiant" required>
+                        <input type="password" class="form-control mb-3" name="motDePasse" placeholder="Mot de passe" required>
+                        <input type="date" class="form-control mb-3" name="dateNaissance" placeholder="Date de naissance" required>
+                        <input type="date" class="form-control mb-3" name="dateEmbauche" placeholder="Date d'embauche" required>
+                        <select class="form-select" name="role" placeholder="Role" required>
+                            <option value="1">Visiteur</option>
+                            <option value="2">Comptable</option>
+                            <option value="3">Administrateur</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                    <button type="submit" class="btn btn-success" name="mode" value="creation">Sauvegarder</button>
+                </div>
                 </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
-                <button type="submit" class="btn btn-success" name="mode" value="creation">Sauvegarder</button>
-            </div>
-            </div>
-        </div>
         </div>
     </form>
     <!-- Modal : Formulaire de création -->
@@ -108,8 +115,19 @@
                             </td>
                             <td><?php echo $utilisateur['libelleRole']; ?></td>
                             <td>
-                                <button class="btn btn-warning"><i class="bi bi-pencil"></i></button>
-                                <button class="btn btn-danger"><i class="bi bi-trash"></i></button>
+                                <!-- Bouton modifier -->
+                                <a href="modifier.php?id=<?php echo $utilisateur['id']; ?>" class="btn btn-warning">
+                                    <i class="bi bi-pencil"></i>
+                                </a>
+
+                                <!-- Bouton supprimer -->
+                                <button 
+                                    class="btn btn-danger" 
+                                    data-bs-toggle="modal" 
+                                    data-bs-target="#suppressionModal" 
+                                    onclick="attribuerIdUtilisateur(<?php echo $utilisateur['id']; ?>)">
+                                    <i class="bi bi-trash"></i>
+                                </button>
                             </td>
                         </tr>
                     <?php
@@ -118,6 +136,40 @@
     </tbody>
     </table>
 </div>
+
+
+<!-- Modal : Formulaire de suppression -->
+<form method="POST" action="">
+    <input type="hidden" id="idUtilisateur" name="idUtilisateur"/>
+
+    <div class="modal fade" id="suppressionModal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Supprimer un utilisateur</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="container">
+                    Etes-vous sur de vouloir supprimer cet utilisateur ?
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                <button type="submit" class="btn btn-danger" name="mode" value="suppression">Supprimer</button>
+            </div>
+            </div>
+        </div>
+    </div>
+</form>
+<!-- Modal : Formulaire de suppression -->
+
+<script>
+    function attribuerIdUtilisateur(id) {
+        document.getElementById('idUtilisateur').value = id;
+    }
+</script>
+
 
 <?php
 require '../includes/footer.php';
